14372559e5f4b8f55117f866077a44fbc125242e
laurenttainturier <laurent.tainturier.pro@gmail.com>
2019-05-01 11:30:39 +0200

    modifying the requests

--- API_Extended_Document/api/web_api.py
+++ API_Extended_Document/api/web_api.py
@@ -53,7 +53,7 @@ def index():
     '''
 
 
-@app.route('/addDocument', methods=['POST'])
+@app.route('/document', methods=['POST'])
 def create_document():
     def creation():
         document = DocController.create_document(
@@ -69,7 +69,47 @@ def create_document():
     return send_response(creation)()
 
 
-@app.route('/addGuidedTour')
+@app.route('/document', methods=['GET'])
+def get_documents():
+    return send_response(
+        lambda: DocController.get_documents(
+            {key: request.args.get(key)
+             for key in request.args.keys()}))()
+
+
+@app.route('/document/<int:doc_id>', methods=['GET'])
+def get_document(doc_id):
+    return send_response(
+        lambda: DocController.get_document_by_id(doc_id))()
+
+
+@app.route('/document/<int:doc_id>', methods=['PUT'])
+def update_document(doc_id):
+    return send_response(
+        lambda: DocController.update_document(doc_id, request.form))()
+
+
+@app.route('/document/<int:doc_id>', methods=['DELETE'])
+def delete_document(doc_id):
+    return send_response(
+        lambda: DocController.delete_documents(doc_id))()
+
+
+@app.route('/document/file/<int:doc_id>', methods=['POST'])
+def upload_file(doc_id):
+    if request.method == 'POST':
+        if request.files.get('file'):
+            file = request.files['file']
+            save_file(doc_id, file)
+
+
+@app.route('/document/file/<int:doc_id>')
+def get_uploaded_file(doc_id):
+    return send_from_directory(
+        safe_join(os.getcwd(), app.config['UPLOAD_FOLDER']), doc_id)
+
+
+@app.route('/guidedtour', methods=['POST'])
 def create_guided_tour():
     name = request.args.get('name')
     description = request.args.get('description')
@@ -80,41 +120,27 @@ def create_guided_tour():
         lambda: TourController.create_tour(name, description))()
 
 
-@app.route('/getDocument/<int:doc_id>', methods=['GET', 'POST'])
-def get_document(doc_id):
-    return send_response(
-        lambda: DocController.get_document_by_id(doc_id))()
-
-
-@app.route('/getGuidedTour/<int:tour_id>')
-def get_guided_tour(tour_id):
-    return send_response(
-        lambda: TourController.get_tour_by_id(tour_id))()
-
-
-@app.route('/getDocuments', methods=['GET', 'POST'])
-def get_documents():
-    return send_response(
-        lambda: DocController.get_documents(
-            {key: request.args.get(key)
-             for key in request.args.keys()}))()
-
-
-@app.route('/getGuidedTours', methods=['GET', 'POST'])
+@app.route('/guidedtour', methods=['GET'])
 def get_all_guided_tours():
     return send_response(
         lambda: TourController.get_tours())()
 
 
-@app.route('/editDocument/<int:doc_id>', methods=['POST'])
-def update_document(doc_id):
+@app.route('/guidedtour/<int:tour_id>')
+def get_guided_tour(tour_id):
     return send_response(
-        lambda: DocController.update_document(doc_id, request.form))()
+        lambda: TourController.get_tour_by_id(tour_id))()
 
 
-@app.route('/addDocumentToGuidedTour')
-def add_document_to_guided_tour():
-    tour_id = request.args.get("tour_id")
+@app.route('/guidedTour/<int:tour_id>', methods=['PUT'])
+def update_guided_tour(tour_id):
+    return send_response(
+        lambda: TourController.update(
+            tour_id, request.form))()
+
+
+@app.route('/guidedTour/<int:tour_id>/document', methods=['POST'])
+def add_document_to_guided_tour(tour_id):
     doc_id = request.args.get('doc_id')
     if doc_id is None or tour_id is None:
         return 'parameter is missing', 400
@@ -123,14 +149,7 @@ def add_document_to_guided_tour():
         lambda: TourController.add_document(tour_id, doc_id))()
 
 
-@app.route('/editGuidedTour/<int:tour_id>', methods=['POST'])
-def update_guided_tour(tour_id):
-    return send_response(
-        lambda: TourController.update(
-            tour_id, request.form))()
-
-
-@app.route('/editGuidedTourDocument/<int:tour_id>', methods=['POST'])
+@app.route('/guidedTour/<int:tour_id>/document', methods=['POST'])
 def update_guided_tour_document(tour_id):
     doc_position = int(request.form.get('doc_position'))
     return send_response(
@@ -138,40 +157,10 @@ def update_guided_tour_document(tour_id):
             tour_id, doc_position, request.form))()
 
 
-@app.route('/deleteGuidedTour/<int:doc_id>')
+@app.route('/guidedTour/<int:doc_id>', methods=['DELETE'])
 def delete_tour(doc_id):
     return send_response(
         lambda: TourController.delete_tour(doc_id))()
-
-
-@app.route('/deleteDocument/<int:doc_id>')
-def delete_document(doc_id):
-    return send_response(
-        lambda: DocController.delete_documents(doc_id))()
-
-
-@app.route('/uploadFile/<int:doc_id>', methods=['GET', 'POST'])
-def upload_file(doc_id):
-    if request.method == 'POST':
-        if request.files.get('file'):
-            file = request.files['file']
-            save_file(doc_id, file)
-
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-
-@app.route('/getFile/<filename>')
-def get_uploaded_file(filename):
-    return send_from_directory(
-        safe_join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
 
 
 if __name__ == '__main__':
